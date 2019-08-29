ee14fb071b7dbc8915e1255881d668adfb733ad8
kjlisby <16364796+kjlisby@users.noreply.github.com>
2019-08-29 21:44:31 +0200

    Animation added for changing between B1 and B2

--- webpage/togbane.js
+++ webpage/togbane.js
@@ -45,3 +45,49 @@ document.getElementById('reset1').onclick = function(){
    document.getElementById('Power1').value = 0;
    output.innerHTML = slider.value;
 }
+
+
+
+var t1s = document.getElementById("T1_S");
+var t2s = document.getElementById("T2_S");
+var t1t = document.getElementById("T1_T");
+var t2t = document.getElementById("T2_T");
+var b1 = document.getElementById("B1");
+var b2 = document.getElementById("B2");
+t1s.onclick = function(){
+ activateB2();
+}
+t2s.onclick = function(){
+ activateB2();
+}
+t1t.onclick = function(){
+ activateB1();
+}
+t2t.onclick = function(){
+ activateB1();
+}
+b1.onclick = function(){
+ activateB1();
+}
+b2.onclick = function(){
+ activateB2();
+}
+function activateB1() {
+ output.innerHTML = "B1 active";
+ t1t.style.visibility="hidden";
+ t2t.style.visibility="hidden";
+ t1s.style.visibility="visible";
+ t2s.style.visibility="visible";
+ b1.src = "skinne_varm.gif";
+ b2.src = "skinne_kold.jpg";
+}
+function activateB2() {
+ output.innerHTML = "B2 active";
+ t1s.style.visibility="hidden";
+ t2s.style.visibility="hidden";
+ t1t.style.visibility="visible";
+ t2t.style.visibility="visible";
+ b2.src = "skinne_varm.gif";
+ b1.src = "skinne_kold.jpg";
+}
+
