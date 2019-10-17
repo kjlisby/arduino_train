98e262a1fa7b9020d0db2ff6b140b5004d89682b
Karl Johan Lisby <klisby@gnhearing.com>
2019-10-17 17:46:22 +0200

    everseLoop scetch compiles but is currently absolutely untested. All IO-pin numbering is known to be bogus.

---
+++ ReverseLoop/PowerSupply.cpp
@@ -0,0 +1,48 @@
+#include "PowerSupply.h"
+#include <Arduino.h>
+
+void PowerSupply::Disable() {
+  this->storedVoltage = this->voltage;
+  this->SetVoltage(0);
+  this->PSUstatus = false;
+}
+
+void PowerSupply::Init(uint8_t DacPin, uint8_t StatusPin, uint8_t RelayPin) {
+	this->dac_pin = DacPin;
+	this->status_pin = StatusPin;
+	this->relay_pin = RelayPin;
+	pinMode(this->relay_pin, OUTPUT);
+	pinMode(this->status_pin, INPUT_PULLUP);
+	
+	this->SetVoltage(0);
+	this->SetPolarity(true);
+}
+
+void PowerSupply::SetVoltage(uint8_t Voltage) {
+	if (this->PSUstatus) {
+		dacWrite(this->dac_pin, Voltage);
+		this->voltage = Voltage;
+	}
+}
+
+void PowerSupply::SetPolarity(bool Polarity) {
+	digitalWrite(this->relay_pin, Polarity);
+	this->polarity = Polarity;
+}
+
+uint8_t PowerSupply::GetVoltage() {
+	return this->voltage;
+}
+
+bool PowerSupply::GetPolarity() {
+	return this->polarity;
+}
+
+bool PowerSupply::GetStatus() {
+	return this->PSUstatus;
+}
+
+void PowerSupply::ResetStatus() {
+	this->PSUstatus = true;
+	this->SetVoltage(this->storedVoltage);
+}
