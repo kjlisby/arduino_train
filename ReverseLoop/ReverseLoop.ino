87b0dfd8e968e85f357dfae1ca36ae4f5653002e
kjlisby <k@l.com>
2019-10-21 14:01:09 +0200

    AjaxHandler made into a proper class, although still just a mockup.

--- ReverseLoop/ReverseLoop.ino
+++ ReverseLoop/ReverseLoop.ino
@@ -1,4 +1,5 @@
 #include "SDWebServer.h"
+#include "AjaxHandler.h"
 #include "PowerSupply.h"
 #include "Turnout.h"
 #include "TrainDetector.h"
@@ -22,6 +23,7 @@
 #define PSU2_ADC_PIN   33
 #define PSU2_POL_PIN   27
 SDWebServer   *WS;
+AjaxHandler   *AH;
 PowerSupply   *PSU1;
 PowerSupply   *PSU2;
 Turnout       *TU;
@@ -29,99 +31,6 @@ TrainDetector *TD1;
 TrainDetector *TD2;
 TrainDetector *TD3;
 TrainDetector *TD4;
-
-/*--------------------------------------------------------------------------------
- * ANOTHER CLASS TO CONTROL SOMETHING
- */
-
-#include <ESP32Servo.h>
-
-#define ledPin LED_BUILTIN
-int counter = 0;
-bool ledState = false;
-int reedState = false;
-
-class MyHandler : public RequestHandler {
-  bool canHandle(HTTPMethod method, String uri) {
-    return uri.startsWith("/ajax_");
-  }
-
-  bool handle(WebServer& server, HTTPMethod requestMethod, String requestUri) {   
-    Serial.println("AJAX GETINPUT");
-    Serial.println(requestUri);
-
-    String cmdarray[10];
-    int index = 0;
-    int startindex = 0;
-    int cmd_length = 0;
-    while (index < requestUri.length()) {
-      if (requestUri[index] == '_') {
-        cmdarray[cmd_length] = requestUri.substring(startindex, index);
-        startindex = index+1;
-        cmd_length++;
-      }
-      index++;
-    }
-    cmdarray[cmd_length] = requestUri.substring(startindex);
-    index++;
-    cmd_length++;
-    
-    for (int i=0; i<cmd_length; i++) {
-      Serial.print(i); Serial.print(": ");
-      Serial.println(cmdarray[i]);
-    }
-
-    if (cmdarray[1].equals("set")) {
-      if (cmdarray[2].equals("B1")) {
-        TU->Throw();
-      }
-      if (cmdarray[2].equals("B2")) {
-        TU->Close();
-      }
-    }
-    server.send(200, "text/plain", "");
-  }
-} myHandler;
-
-boolean LEDstate = false;
-void loopHandler() {
-    bool old_reedState = reedState;
-    reedState = TD1->TrainSeen();
-    if (old_reedState != reedState) {
-      if (reedState) {
-        analogWrite(ledPin,255);
-        PSU1->ResetStatus();
-        PSU1->SetSpeed(-255);
-      } else {
-        analogWrite(ledPin,25);
-        PSU1->SetSpeed(0);
-      }
-    }
-    counter++;
-    if (counter > 500000) {
-      counter = 0;
-      Serial.print("TOGGLE LED ");
-//      digitalWrite (ledPin,!digitalRead(ledPin));
-      if (LEDstate) {
-        Serial.println("OFF");
-        LEDstate = false;
-        analogWrite(ledPin,0);
-      } else {
-        Serial.println("ON");
-        LEDstate = true;
-        analogWrite(ledPin,100);
-      }
-    }
-}
-
-void initHandler() {
-  pinMode ( ledPin, OUTPUT );
-  analogWrite ( ledPin, 0 );
-}
-
-/*
- * -----------------------------------------------------------------------------------------------------------------------
- */
 
 
 void InitPowerSupplies() {
@@ -159,15 +68,15 @@ void InitTrainDetectors () {
 	TD1 = new TrainDetector();
 	TD1->Init(TRAIN_SENSOR1, &trainDetectionMillis1);
 	attachInterrupt(digitalPinToInterrupt(TRAIN_SENSOR1), handleTrainDetectorInterrupt1, FALLING);
-	TD2 = new TrainDetector();
-	TD2->Init(TRAIN_SENSOR2, &trainDetectionMillis2);
-	attachInterrupt(digitalPinToInterrupt(TRAIN_SENSOR2), handleTrainDetectorInterrupt2, FALLING);
-	TD3 = new TrainDetector();
-	TD3->Init(TRAIN_SENSOR3, &trainDetectionMillis3);
-	attachInterrupt(digitalPinToInterrupt(TRAIN_SENSOR3), handleTrainDetectorInterrupt3, FALLING);
-	TD4 = new TrainDetector();
-	TD4->Init(TRAIN_SENSOR4, &trainDetectionMillis4);
-	attachInterrupt(digitalPinToInterrupt(TRAIN_SENSOR4), handleTrainDetectorInterrupt4, FALLING);
+	// TD2 = new TrainDetector();
+	// TD2->Init(TRAIN_SENSOR2, &trainDetectionMillis2);
+	// attachInterrupt(digitalPinToInterrupt(TRAIN_SENSOR2), handleTrainDetectorInterrupt2, FALLING);
+	// TD3 = new TrainDetector();
+	// TD3->Init(TRAIN_SENSOR3, &trainDetectionMillis3);
+	// attachInterrupt(digitalPinToInterrupt(TRAIN_SENSOR3), handleTrainDetectorInterrupt3, FALLING);
+	// TD4 = new TrainDetector();
+	// TD4->Init(TRAIN_SENSOR4, &trainDetectionMillis4);
+	// attachInterrupt(digitalPinToInterrupt(TRAIN_SENSOR4), handleTrainDetectorInterrupt4, FALLING);
 }
 
 void setup(void) {
@@ -176,13 +85,13 @@ void setup(void) {
   Serial.print("\n");
   WS = new SDWebServer();
   WS->getServer()->onNotFound(SDWebServer_handleNotFound);
-  WS->getServer()->addHandler(&myHandler);
+  AH = new AjaxHandler();
+  AH->Init(WS->getServer());
   WS->Init(SD_CS_PIN);
   InitPowerSupplies();
   TU = new Turnout();
   TU->Init(TURNOUT1_PIN);
   InitTrainDetectors();
-  initHandler();
 }
 
 void loop(void) {
@@ -190,5 +99,5 @@ void loop(void) {
   PSU1->Loop();
   PSU2->Loop();
   TU->Loop();
-  loopHandler();
+  AH->Loop();
 }
